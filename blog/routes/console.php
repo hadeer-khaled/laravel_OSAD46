105bf86d594f8bd2d9ad3451cfa3a10cc87b0e8e
hadeer_khaled <hadeer_khaled@etax.com.eg>
2026-05-15 19:57:46 +0300

    task schedule

--- blog/routes/console.php
+++ blog/routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+  $count =  Post::onlyTrashed()->where('deleted_at', '<=', now()->subDays(30))->forceDelete();
+  \Log::info("Deleted $count posts that were soft deleted more than 30 days ago.");
+})->everyTwoSeconds();
